cae3c07e06b15a53ea0a88f108b5af45c0c57abb
aymm <max.nuding@icloud.com>
2020-10-04 17:35:31 +0000

    Fixed #1

--- Duplicate.novaextension/CHANGELOG.md
+++ Duplicate.novaextension/CHANGELOG.md
@@ -1,3 +1,7 @@
+## Version 1.0.2
+
+* Fixed cursor position after duplicating a complete line
+
 ## Version 1.0.1
 
 Added license
--- Duplicate.novaextension/Scripts/main.js
+++ Duplicate.novaextension/Scripts/main.js
@@ -7,15 +7,40 @@ exports.deactivate = function() {
 }
 
 
-nova.commands.register("duplicate.duplicateSelection", (editor) => {
-    var selectedRanges = editor.selectedRanges.reverse();
+nova.commands.register("duplicate.duplicateSelection", async (editor) => {
+    // Selections, from the bottom of the document to the top
+    let selectedRanges = editor.selectedRanges.sort((r1, r2) => r2.start - r1.start);
+    const originalSelection = selectedRanges;
+
     // Selects the whole line for all "empty" ranges (ranges without selection, only a cursor position)
     selectedRanges = selectedRanges.map(r => r.empty ? editor.getLineRangeForRange(r) : r);
 
-    editor.edit(function(e) {
-        for (var range of selectedRanges) {
-            var text = editor.getTextInRange(range);
-            e.insert(range.end, text);
+    const lengths = selectedRanges.map(r => r.length);
+    let newSelection = [];
+    await editor.edit(function(e) {
+        for (const [index, range] of selectedRanges.entries()) {
+            const text = editor.getTextInRange(range);
+            console.log(text);
+            e.replace(range, text + text);
+            const originalRange = originalSelection[index];
+
+            // Move the selection/cursor to the newly created text
+            newSelection.push(new Range(originalRange.start + text.length, originalRange.end + text.length));
         }
     });
+
+    /*
+     * Adding text causes the selections to be off.
+     * This moves all selections (starting at the top of the document)
+     * by the summed length of all added text before it, so all selections end up being at the same position,
+     * but in the newly added text
+     */
+    newSelection.reverse();
+    lengths.reverse();
+    let totalLength = 0;
+    for (const [i, r] of newSelection.entries()) {
+        newSelection[i] = new Range(r.start + totalLength, r.end + totalLength);
+        totalLength += lengths[i];
+    }
+    editor.selectedRanges = newSelection;
 });
